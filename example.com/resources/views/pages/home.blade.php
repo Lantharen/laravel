271ccc1229d0a66b3229c38a5a2e1e5c611e67a9
Lantharen <97023561+Lantharen@users.noreply.github.com>
2022-11-22 18:24:10 +0200

    full

---
+++ example.com/resources/views/pages/home.blade.php
@@ -0,0 +1,163 @@
+@extends('layouts.base')
+@section('page-title', 'Marathon Digital Holdings – Building America\'s Leading Enterprise Bitcoin Miner')
+@section('page-class', 'home page-template-default page page-id-5 fl-builder fl-theme-builder-header fl-theme-builder-header-header fl-theme-builder-footer fl-theme-builder-footer-footer fl-framework-base fl-preset-default fl-full-width fl-search-active fl-builder-breakpoint-large')
+@section('page-content')
+    <div class="fl-page">
+
+        @include('pages.includes.header')
+
+        <div class="uabb-js-breakpoint" style="display: none;"></div>
+        <div id="fl-main-content" class="fl-page-content" itemprop="mainContentOfPage" role="main">
+
+
+            <div class="fl-content-full container">
+                <div class="row">
+                    <div class="fl-content col-md-12">
+                        <article class="fl-post post-5 page type-page status-publish hentry" id="fl-post-5"
+                                 itemscope="itemscope" itemtype="https://schema.org/CreativeWork">
+
+                            <div class="fl-post-content clearfix" itemprop="text">
+                                <div
+                                    class="fl-builder-content fl-builder-content-5 fl-builder-content-primary fl-builder-global-templates-locked"
+                                    data-post-id="5">
+                                    <div id="home-hero"
+                                         class="fl-row fl-row-full-width fl-row-bg-parallax fl-node-603ce1daa6c03 fl-row-full-height fl-row-align-center desktop-only-background"
+                                         data-node="603ce1daa6c03" data-parallax-speed="2"
+                                         data-parallax-image="{{ asset('/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored.jpg') }}">
+                                        <div class="fl-row-content-wrap"
+                                             style="background-position: center 62.5px; background-image: url({{ asset('/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored.jpg') }});">
+                                            <div class="fl-row-content fl-row-full-width fl-node-content">
+
+                                                <div class="fl-col-group fl-node-603ce1daa8d39"
+                                                     data-node="603ce1daa8d39">
+                                                    <div class="fl-col fl-node-603ced2d8be46 fl-col-small"
+                                                         data-node="603ced2d8be46">
+                                                        <div class="fl-col-content fl-node-content">
+                                                            <div
+                                                                class="fl-module fl-module-photo fl-node-603ced0be5b66 fl-visible-medium"
+                                                                data-node="603ced0be5b66">
+                                                                <div class="fl-module-content fl-node-content">
+                                                                    <div class="fl-photo fl-photo-align-center"
+                                                                         itemscope=""
+                                                                         itemtype="https://schema.org/ImageObject">
+                                                                        <div class="fl-photo-content fl-photo-img-jpg">
+                                                                            <img decoding="async"
+                                                                                 class="fl-photo-img wp-image-313 size-full"
+                                                                                 src="https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=1080%2C1080&amp;ssl=1"
+                                                                                 alt="Graphical representation room full of Bitcoin miners"
+                                                                                 itemprop="image" height="1080"
+                                                                                 width="1080" title="Bitcoin Miners"
+                                                                                 srcset="https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?w=1080&amp;ssl=1 1080w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=300%2C300&amp;ssl=1 300w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=1024%2C1024&amp;ssl=1 1024w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=150%2C150&amp;ssl=1 150w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=768%2C768&amp;ssl=1 768w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=900%2C900&amp;ssl=1 900w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=600%2C600&amp;ssl=1 600w"
+                                                                                 sizes="(max-width: 1000px) 100vw, 1000px">
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div
+                                                                class="fl-module fl-module-photo fl-node-603cf1023acf9 fl-visible-mobile"
+                                                                data-node="603cf1023acf9">
+                                                                <div class="fl-module-content fl-node-content">
+                                                                    <div class="fl-photo fl-photo-align-center"
+                                                                         itemscope=""
+                                                                         itemtype="https://schema.org/ImageObject">
+                                                                        <div class="fl-photo-content fl-photo-img-jpg">
+                                                                            <img decoding="async"
+                                                                                 class="fl-photo-img wp-image-313 size-full"
+                                                                                 src="https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=1080%2C1080&amp;ssl=1"
+                                                                                 alt="Graphical representation room full of Bitcoin miners"
+                                                                                 itemprop="image" height="1080"
+                                                                                 width="1080" title="Bitcoin Miners"
+                                                                                 srcset="https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?w=1080&amp;ssl=1 1080w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=300%2C300&amp;ssl=1 300w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=1024%2C1024&amp;ssl=1 1024w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=150%2C150&amp;ssl=1 150w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=768%2C768&amp;ssl=1 768w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=900%2C900&amp;ssl=1 900w, https://i0.wp.com/marathondh.com/wp-content/uploads/2021/03/miner-array_2021-03-05_16-35__0001_recolored_1x1.jpg?resize=600%2C600&amp;ssl=1 600w"
+                                                                                 sizes="(max-width: 1000px) 100vw, 1000px">
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div
+                                                        class="fl-col fl-node-605b1e7d9d687 fl-col-small mdh-text-column"
+                                                        data-node="605b1e7d9d687">
+                                                        <div class="fl-col-content fl-node-content">
+                                                            <div
+                                                                class="fl-module fl-module-rich-text fl-node-605b1e7d9d7de fl-animation fl-fade-right fl-animated"
+                                                                data-node="605b1e7d9d7de" data-animation-delay="0"
+                                                                data-animation-duration="1"
+                                                                style="animation-duration: 1s;">
+                                                                <div class="fl-module-content fl-node-content">
+                                                                    <div class="fl-rich-text">
+                                                                        <h1><strong>Supporting the Bitcoin
+                                                                                ecosystem</strong></h1>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div
+                                                                class="fl-module fl-module-rich-text fl-node-605b1e7d9d7e0"
+                                                                data-node="605b1e7d9d7e0">
+                                                                <div class="fl-module-content fl-node-content">
+                                                                    <div class="fl-rich-text">
+                                                                        <p>At Marathon, we aim to facilitate greater
+                                                                            transparency, financial inclusion, and
+                                                                            economic freedom by enhancing the
+                                                                            development and security of the world’s
+                                                                            first public blockchain ledger, one block at
+                                                                            a time. We believe in a more open,
+                                                                            inclusive, and fair financial system, and we
+                                                                            believe Bitcoin is key to making it
+                                                                            happen.</p>
+                                                                        <p>&nbsp;</p>
+                                                                        <h3><strong>#ThinkOutsideTheBlocks</strong></h3>
+                                                                        <p>We’re different. We're currently in the
+                                                                            process of building one of the largest, most
+                                                                            agile, and most sustainably powered Bitcoin
+                                                                            server operations in North America, while
+                                                                            remaining asset light. Rather than building
+                                                                            and operating data centers, we focus on
+                                                                            increasing our computing power (hash rate)
+                                                                            and becoming more energy efficient while
+                                                                            staying lean and staying green.</p>
+                                                                        <p>A key component of our strategy is to
+                                                                            co-locate our operations at sustainable
+                                                                            energy sites, when feasible, to ensure our
+                                                                            power source is as sustainable as possible
+                                                                            and to reduce the stresses placed on our
+                                                                            electrical grid infrastructure.</p>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div
+                                                                class="fl-module fl-module-rich-text fl-node-605b1e7d9d7e2"
+                                                                data-node="605b1e7d9d7e2">
+                                                                <div class="fl-module-content fl-node-content">
+                                                                    <div class="fl-rich-text">
+                                                                        <p>Statements on this page are based solely upon
+                                                                            management's intentions.&nbsp; There is no
+                                                                            guarantee that results will be achieved, and
+                                                                            you should conduct your own evaluation as to
+                                                                            the achievability of results.</p>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="uabb-js-breakpoint" style="display: none;"></div>
+                            </div><!-- .fl-post-content -->
+
+                        </article>
+
+                        <!-- .fl-post -->
+                    </div>
+                </div>
+            </div>
+
+
+        </div><!-- .fl-page-content -->
+
+        <!-- fl-footer -->
+    @include('pages.includes.footer')
+@endsection
